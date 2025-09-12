a4a3c4bf44632b88a76ed40854c273d52d9796db
MateoTVara <146038574+MateoTVara@users.noreply.github.com>
2025-09-12 17:04:36 -0500

    implement basic project board UI and add project dialog
    - Add initial layout in template.html: header, board, add project button, and dialog form
    - Style board, project items, header, and dialog in styles.css
    - Create Project class and managers in index.js for adding/displaying projects
    - Enable adding new projects via dialog and rendering them on the board

--- src/index.js
+++ src/index.js
@@ -1,3 +1,86 @@
 // index.js
 
 import "./styles.css";
+
+class Project {
+  constructor(name) {
+    this.name = name;
+    this.color = this.#generateRandomColor();
+  }
+
+  #generateRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`
+  }
+
+  #handleClick() {
+    console.log(`Project: ${this.name}`);
+  }
+
+  getElement () {
+    const div = document.createElement("div");
+    div.classList.add("project-item");
+    div.setAttribute("title", this.name)
+
+    div.addEventListener("click", () => this.#handleClick());
+
+    const colorDiv = document.createElement("div");
+    colorDiv.style.backgroundColor = this.color;
+
+    const h2 = document.createElement("h2");
+    h2.textContent = this.name;
+
+    div.append(colorDiv, h2);
+
+    return div;
+  }
+}
+
+const DialogManager = (function() {
+  const addProjectDialog = document.querySelector(".add-project-dialog");
+  const addProjectFormButton = document.querySelector(".add-project-dialog form button");
+  const addProjectFormInput = document.querySelector(".add-project-dialog form input");
+
+  const toggleDialogDisplay = () => {
+    if (addProjectDialog.style.display !== "flex") {
+      addProjectDialog.style.display = "flex"
+    } else {
+      addProjectDialog.style.display = "none";
+    }
+  }
+
+  addProjectDialog.addEventListener("click", (e) => {
+    if (e.target === addProjectDialog) {
+      toggleDialogDisplay();
+    }
+  })
+
+  addProjectFormButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (addProjectFormInput.value !== "") {
+      const project = new Project(addProjectFormInput.value);
+      ProjectsManager.getBoard().insertBefore(project.getElement(), ProjectsManager.getAddProjectDiv());
+      addProjectDialog.style.display = "none";
+      addProjectFormInput.value = "";
+    }
+  })
+
+  return { toggleDialogDisplay };
+})();
+
+const ProjectsManager = (function() {
+  const board = document.querySelector(".board");
+  const addProjectDiv = document.querySelector(".add-project");
+  
+  addProjectDiv.addEventListener("click", () => {
+    DialogManager.toggleDialogDisplay();
+  });
+
+  const getBoard = () => board;
+  const getAddProjectDiv = () => addProjectDiv;
+
+  return { getBoard, getAddProjectDiv };
+})();
+
